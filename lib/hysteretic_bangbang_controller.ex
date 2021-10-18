01efcbbb3823aed5087abd0babdff769e46a6b3c
crertel <chris@kedagital.com>
2021-10-18 03:10:07 -0500

    Use pattern-matching more for HBBC.

--- lib/hysteretic_bangbang_controller.ex
+++ lib/hysteretic_bangbang_controller.ex
@@ -3,18 +3,14 @@ defmodule ControlLoop.HystereticBangBangController do
 
   Record.defrecord(:hbb_controller, previous_state: :off, setpoint_start: 0, setpoint_end: 0,  output_off: 0, output_on: 0)
 
-  def update( hbb_controller(previous_state: :off, setpoint_start: setpoint_start, setpoint_end: setpoint_end, output_off: output_off, output_on: output_on) = controller, input) do
-    if input > setpoint_end do
-      {output_on, hbb_controller(controller, previous_state: :on)}
-    else
-      {output_off, controller}
-    end
+  def update( hbb_controller(previous_state: :off, setpoint_end: setpoint_end, output_on: output_on) = controller, input)
+    when input > setpoint_end  do {output_on, hbb_controller(controller, previous_state: :on)} end
+  def update( hbb_controller(previous_state: :off, output_off: output_off) = controller, _) do
+    {output_off, controller}
   end
-  def update( hbb_controller(previous_state: :on, setpoint_start: setpoint_start, setpoint_end: setpoint_end, output_off: output_off, output_on: output_on) = controller, input) do
-    if input < setpoint_start do
-      {output_off, hbb_controller(controller, previous_state: :off)}
-    else
-      {output_on, controller}
-    end
+  def update( hbb_controller(previous_state: :on, setpoint_start: setpoint_start, output_off: output_off) = controller, input)
+    when input < setpoint_start do {output_off, hbb_controller(controller, previous_state: :off)} end
+  def update( hbb_controller(previous_state: :on, output_on: output_on) = controller, _) do
+    {output_on, controller}
   end
 end
