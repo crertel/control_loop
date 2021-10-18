6727d1755577fa9462594af2ce2fe892cf915bab
crertel <chris@kedagital.com>
2021-10-18 03:13:23 -0500

    Better pattern-matching for BBC.

--- lib/bangbang_controller.ex
+++ lib/bangbang_controller.ex
@@ -3,9 +3,7 @@ defmodule ControlLoop.BangBangController do
 
   Record.defrecord(:bb_controller, setpoint: 0, output_off: 0, output_on: 0)
 
-  def update( bb_controller(setpoint: setpoint, output_off: output_off, output_on: output_on) = controller, input) do
-    output = if input > setpoint, do: output_on, else: output_off
-
-    {output, controller}
-  end
+  def update( bb_controller(setpoint: setpoint, output_off: output_off) = controller, input)
+    when input <= setpoint do {output_off, controller} end
+  def update( bb_controller(output_on: output_on) = controller, _),  do: {output_on, controller}
 end
